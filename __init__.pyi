0855c3e07dac0b68f4c88d165ebe6857b148a69b
OkuharaJoy <Joy1031sparky@gmail.com>
2025-02-07 10:25:46 +0900

    lib/site-packages/Crypto/Util

--- __init__.pyi
+++ __init__.pyi
@@ -1,19 +1,4 @@
-from typing import Any
+from typing import Tuple, Union
 
-__all__ = ['new', 'get_random_bytes']
-
-from os import urandom
-
-class _UrandomRNG(object):
-
-    def read(self, n: int) -> bytes:...
-    def flush(self) -> None: ...
-    def reinit(self) -> None: ...
-    def close(self) -> None: ...
-
-def new(*args: Any, **kwargs: Any) -> _UrandomRNG: ...
-
-def atfork() -> None: ...
-
-get_random_bytes = urandom
-
+version_info : Tuple[int, int, Union[int, str]]
+__version__ : str
