e7c1c5c857ca9bb54e174e3b603f55e3e5678926
OkuharaJoy <Joy1031sparky@gmail.com>
2025-02-07 10:35:36 +0900

    lib/site-packages//pip/_internal/metadata

--- commands/help.py
+++ commands/help.py
@@ -1,17 +1,41 @@
-#!/usr/bin/env python
-# encoding: utf-8
+from optparse import Values
+from typing import List
 
-import Naked.settings
-from Naked.toolshed.system import exit_success
-
-class Help:
-    def __init__(self):
-        self.help = Naked.settings.help
-
-    def print_help(self):
-        print(self.help)
-        exit_success()
+from pip._internal.cli.base_command import Command
+from pip._internal.cli.status_codes import SUCCESS
+from pip._internal.exceptions import CommandError
 
 
-if __name__ == '__main__':
-    pass
+class HelpCommand(Command):
+    """Show help for commands"""
+
+    usage = """
+      %prog <command>"""
+    ignore_require_venv = True
+
+    def run(self, options: Values, args: List[str]) -> int:
+        from pip._internal.commands import (
+            commands_dict,
+            create_command,
+            get_similar_commands,
+        )
+
+        try:
+            # 'pip help' with no args is handled by pip.__init__.parseopt()
+            cmd_name = args[0]  # the command we need help for
+        except IndexError:
+            return SUCCESS
+
+        if cmd_name not in commands_dict:
+            guess = get_similar_commands(cmd_name)
+
+            msg = [f'unknown command "{cmd_name}"']
+            if guess:
+                msg.append(f'maybe you meant "{guess}"')
+
+            raise CommandError(" - ".join(msg))
+
+        command = create_command(cmd_name)
+        command.parser.print_help()
+
+        return SUCCESS
